97a01c46e5ffc01041f94f761b3a231f81daec3c
OrangeBurrito <mali.kewl.pool@gmail.com>
2022-05-17 13:17:04 +0700

    Move bookingPreview to Booking component, replace label/input with FormInput

---
+++ src/lib/components/Booking.svelte
@@ -0,0 +1,122 @@
+<script>
+	import FormInput from '$lib/components/FormInput.svelte'
+
+	export let fullPage = false;
+
+	function onSubmitBookingPreview(e) {
+		const formData = new FormData(e.target);
+
+		const data = {}
+		for (let field of formData) {
+			const [key,value] = field
+			data[key] = value
+		}
+
+		console.log(data)
+	}
+</script>
+
+{#if fullPage}
+<form action="">
+	<section>
+		<h2>whene</h2>
+		<FormInput type="date" id="date" name="date" required>on:</FormInput>
+		<div>
+			<label for="session">for a:</label>
+			<abbr title="required">*</abbr>
+			<select id="session" name="session-type" required>
+				<option value="morning">Morning Session</option>
+				<option value="afternoon">Afternoon Session</option>
+				<option value="evening">Evening Session</option>
+			</select>
+		</div>
+		<div>
+			<FormInput type="number" id="students" name="number-of-students" placeholder="2">for:</FormInput>
+			<label for="students">for:</label>
+			<abbr title="required">*</abbr>
+			<input type="number" id="students" name="number-of-students" placeholder="2" min="1" max="10" required> -->
+			<span>students</span>
+		</div>
+	</section>
+	
+	<section>
+		<h2>who</h2>
+		<FormInput type="text" id="full-name" name="full-name" required>full name:</FormInput>
+		<FormInput type="email" id="email" name="email" required>email:</FormInput>
+		<FormInput type="tel" id="phone-number" name="phone-number">phone number:</FormInput>
+	</section>
+
+	<section>
+		<h2>what and why</h2>
+			
+		<fieldset>
+			<legend>Dishes to make</legend>
+			<p>(choose up to three dishes)</p>
+
+			<div>
+				<label for="search-food">Search</label>
+				<input type="search" id="search-food" name="search-food" placeholder="Search for dishes">
+				<select name="" id="">
+					<option value="dishes">Dishes</option>
+					<option value="desserts">Desserts</option>
+				</select>
+			</div>
+
+			<div>
+				<h3>Dishes</h3>
+				<div>
+					<input type="checkbox" id="food1" name="food1">
+					<label for="food1">Food1</label>
+				</div>
+			</div>
+
+			<div>
+				<h3>Desserts</h3>
+				<div>
+					<input type="checkbox" id="food1" name="food1">
+					<label for="food1">Food1</label>
+				</div>
+				<div>
+					<input type="checkbox" id="food2" name="food2">
+					<label for="food2">Food2</label>
+				</div>
+				<div>
+					<input type="checkbox" id="food3" name="food3">
+					<label for="food3">Food3</label>
+				</div>
+			</div>
+		</fieldset>
+	
+		<div>
+			<label for="comments">Additional comments:</label>
+			<textarea id="comments" name="comments"></textarea>
+		</div>
+	</section>
+
+	<button type="submit">Submit</button>
+</form>
+{:else}
+<div class="booking-preview">
+	<h2>Book a cooking class</h2>
+	<p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+
+	<form action="/booking" method="get" on:submit|preventDefault={onSubmitBookingPreview}>
+		<FormInput type="date" id="date" name="date" label="on:" required/>
+		<FormInput type="select" id="session" name="session-type" required>
+			<option value="morning">Morning Session</option>
+			<option value="afternoon">Afternoon Session</option>
+			<option value="evening">Evening Session</option>
+		</FormInput>
+		<FormInput type="number" id="students" name="number-of-students" placeholder="2" min=1 max=10 label="for:" required>students</FormInput>
+		<div>
+		<button type="submit">Submit</button>
+	</form>
+</div>
+{/if}
+
+<style>
+	.booking-preview {
+		background: var(--primary-variant);
+		color: var(--on-primary-variant);
+	}
+</style>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,107 +2,110 @@
   import Dishes from "$lib/components/Dishes.svelte";
   import Features from "$lib/components/Features.svelte";
   import Reviews from "$lib/components/reviews/Reviews.svelte";
+  import Gallery from "$lib/components/Gallery.svelte";
+  import Contact from "$lib/components/Contact.svelte";
+  import Booking from "$lib/components/Booking.svelte";
   import reviews from "$lib/reviews.json";
-	import Gallery from "$lib/components/Gallery.svelte";
-	import Contact from "$lib/components/Contact.svelte";
-import BookingPreview from "$lib/components/BookingPreview.svelte";
-
-	let menuCategory = 'dishes';
+	
+  let menuCategory = "dishes";
 </script>
 
 <div class="call-to-action">
   <h1>Private Thai Home Cooking Course in Bangkok</h1>
   <p>Learn how to cook authentic Thai food in my home kitchen.</p>
-	<button>
-		<a href="/#booking">Book an appointment</a>
-	</button>
+  <button>
+    <a href="/#booking">Book an appointment</a>
+  </button>
 </div>
 
 <Features />
 
-
 <div class="about">
-	<h2>Thai Cooking Classes</h2>
+  <h2>Thai Cooking Classes</h2>
   <p>Learn how to cook authentic Thai food in my home kitchen.</p>
   <p>
-		Unlike traditional cooking schools, I hold private Thai cooking lessons in English or in Thai. You choose day, time, and the dishes you want to cook from my
+    Unlike traditional cooking schools, I hold private Thai cooking lessons in English or in Thai. You choose day, time, and the dishes you want to cook from my
     family cookbook.
   </p>
   <p>Each lesson covers three dishes of your choice. The dishes are listed below, but you may also request dishes not listed in the dishes menu.</p>
   <p>All ingredients are included along with copies of the recipes in English.</p>
-	<div class="about-image"/>
+  <div class="about-image" />
 </div>
 
 <div class="menu">
-	<h1>Menus</h1>
-	
-	<div class="category-btns">
-		<button class="ghost-btn" class:selected={menuCategory==='dishes'} on:click={()=> menuCategory = 'dishes'}>Dishes</button>
-		<button class="ghost-btn" class:selected={menuCategory==='desserts'} on:click={()=> menuCategory = 'desserts'}>Desserts</button>
-	</div>
-	
-	<Dishes selected={menuCategory}/>
+  <h1>Menus</h1>
+
+  <div class="category-btns">
+    <button class="ghost-btn" class:selected={menuCategory === "dishes"} on:click={() => (menuCategory = "dishes")}>Dishes</button>
+    <button class="ghost-btn" class:selected={menuCategory === "desserts"} on:click={() => (menuCategory = "desserts")}>Desserts</button>
+  </div>
+
+  <Dishes selected={menuCategory} />
 </div>
 
-<BookingPreview/>
+<Booking />
 
 <Reviews {reviews} />
 
-<Gallery/>
+<Gallery />
 
-<Contact/>
+<Contact />
 
 <style>
-	.call-to-action {
-		text-align: center;
-		background-image: url('https://images.unsplash.com/photo-1610452220299-5edf90b8a6ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80');
-		background-size: cover;
-		background-position: center;
-		color: var(--on-background-image);
-		padding: 2rem 1rem;
-	}
+  .call-to-action {
+    text-align: center;
+    background-image: url("https://images.unsplash.com/photo-1610452220299-5edf90b8a6ed?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80");
+    background-size: cover;
+    background-position: center;
+    color: var(--on-background-image);
+    padding: 2rem 1rem;
+  }
 
-	.call-to-action p {
-		padding: 1rem 0;
-	}
+  .call-to-action p {
+    padding: 1rem 0;
+  }
 
-	.about {
-		display: flex;
-		flex-direction: column;
-		gap: 1rem;
-		background: var(--surface);
-		color: var(--on-surface);
-		padding: 1.5rem;
-	}
+  .about {
+    display: flex;
+    flex-direction: column;
+    gap: 1rem;
+    background: var(--surface);
+    color: var(--on-surface);
+    padding: 1.5rem;
+  }
 
-	.about p {
-		color: var(--on-surface-light);
-	}
+  .about p {
+    color: var(--on-surface-light);
+  }
 
-	.about-image {
-		background-image: url('/images/angsana.jpg');
-		background-position: center;
-		background-size: cover;
-		width: 200px;
-		height: 150px;
-		margin: 0 auto;
-		padding-top: 1rem;
-	}
+  .about-image {
+    background-image: url("/images/angsana.jpg");
+    background-position: center;
+    background-size: cover;
+    width: 200px;
+    height: 150px;
+    margin: 0 auto;
+    padding-top: 1rem;
+  }
 
-	.menu { padding: 2rem; }
+  .menu {
+    padding: 2rem;
+  }
 
-	.menu h1 { text-align: center }
+  .menu h1 {
+    text-align: center;
+  }
 
-	.menu .category-btns {
-		display: flex;
-		justify-content: center;
-		gap: 1rem;
-	}
+  .menu .category-btns {
+    display: flex;
+    justify-content: center;
+    gap: 1rem;
+  }
 
-	.menu .category-btns .selected {
-		color: var(--primary-variant);
-		text-decoration: underline;
-		font-size: 1.1em;
-		font-weight: bold;
-	}
+  .menu .category-btns .selected {
+    color: var(--primary-variant);
+    text-decoration: underline;
+    font-size: 1.1em;
+    font-weight: bold;
+  }
 </style>
